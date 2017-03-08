18552bc241aa75af3013241f1ff20e04e9b7e0d7
Andrew Taylor <agrahamt@Andrews-MacBook-Air.local>
2017-03-08 17:17:52 -0600

    completed exercise03

---
+++ exercise03.js
@@ -0,0 +1,20 @@
+// a:
+var result = "The cat jumped over the moon";
+//string
+
+// b:
+var result = 55 - 27;
+//numbe
+
+// c:
+var result = 75 > 55;
+//boolean
+
+// d:
+var data;
+var result = data;
+//string
+
+// e:
+var result = -6544.432;
+//number
