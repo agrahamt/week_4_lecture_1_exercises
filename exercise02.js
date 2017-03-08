82365a163aac3f809d8ef31eea2a8d46d5959e47
Andrew Taylor <agrahamt@Andrews-MacBook-Air.local>
2017-03-08 16:50:53 -0600

    added Exercise02

---
+++ exercise02.js
@@ -0,0 +1,10 @@
+var firstName = "Jack";
+var lastName = "Smith";
+
+function greet (first, last) {
+  return "Good morning, " + first + " " last
+}
+//a function declaration doesn't end with a semicolon
+greet(firstName, lastName);
+//however when you call a function
+//the line ends with a semicolon.
