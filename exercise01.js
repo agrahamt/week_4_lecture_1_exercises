5ed80209505b6b38b79dfea86d4216fe1116b393
Andrew Taylor <agrahamt@Andrews-MacBook-Air.local>
2017-03-08 16:41:10 -0600

    added exercise01

---
+++ exercise01.js
@@ -0,0 +1,10 @@
+var jenny = "8675309";
+//this declares the variable named Jenny and
+//assigns it the value of the string in quotes.
+//If there were no quotation marks,
+//the type would be a number.
+function number-on-the-wall (str) {
+  return str + " - I got it! I got it!"
+}
+//this function concatenates
+//any input argument with the string.
